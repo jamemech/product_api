4fe96e355ef910257ef07b2cc09a3d14308ece69
jamemech <jamemech01@gmail.com>
2024-09-07 16:40:05 +0700

    Set up productapi

---
+++ src/db/sequelize.js
@@ -0,0 +1,45 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+module.exports = {
+  development: {
+    storage: './dev.sqlite3',
+    dialect: 'sqlite'
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
+  },
+  staging: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mssql',
+    dialectOptions: {
+      option: {
+        validateBulkLoadParameters: false
+      }
+    }
+  }
+}
